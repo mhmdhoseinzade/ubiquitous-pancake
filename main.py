45b017cb7569df2a50801a8d8a0e7b818dd3a081
Mohammad Hoseinzade <m.hoseinzadeh91@gmail.com>
2023-07-20 09:31:51 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return 'Web App with Python Flask! version one '
 
 app.run(host='0.0.0.0', port=5000)
